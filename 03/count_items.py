5a2af3f82e6e49467e45ef301695f0c2d31fda1b
gnoc <lflaack@protonmail.com>
2022-12-03 10:40:43 +0100

    03: Solve puzzle 02

--- 03/count_items.py
+++ 03/count_items.py
@@ -12,22 +12,41 @@ def get_doubled_items(inp: str) -> list:
                 break
     return doubled
 
+def get_badges(inp: str) -> list:
+    badges = []
+    lines = inp.split("\n")
+    for i in range(0, len(lines), 3): # len(lines) is guaranteed to be wholly divisible by 3
+        found = False # pinnacle of inelegance
+        for c1 in lines[i]:
+            if found:
+                break
+            for c2 in lines[i + 1]:
+                if found:
+                    break
+                if c1 == c2:
+                    for c3 in lines[i + 2]:
+                        if c1 == c3:
+                            found = True
+                            badges.append(c1)
+                            break
+    return badges
+
 def get_priority(inp: str) -> int:
     if inp in alphabet:
         return ord(inp) - 96
     if inp in alphabet.upper():
         return ord(inp) - 38
-    error("oops")
+    print("hwaet? {inp}")
 
 
 
 if __name__ == "__main__":
     testinput = """vJrwpWtwJgWrhcsFMMfFFhFp
-    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-    PmmdzqPrVvPwwTWBwg
-    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-    ttgJtRGJQctTZtZT
-    CrZsJsPPZsGzwwsLwLmpwMDw"""
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
 
     with open("03/input.txt") as file:
-        print(sum(map(get_priority, get_doubled_items(file.read()))))
+        print(sum(map(get_priority, get_badges(file.read()))))
