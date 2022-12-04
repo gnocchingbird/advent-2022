5d2e1505dd10c7daf5f4596f48c280fc351f13a4
gnoc <lflaack@protonmail.com>
2022-12-04 16:10:30 +0100

    04: Solve puzzle 01

---
+++ 04/clean_sections.py
@@ -0,0 +1,22 @@
+
+def get_section_ids(inp: str) -> list:
+    """Returns a list of 2-element-lists each containing the assignments for the first and the second elf"""
+    range_ = lambda start, end: range(start, end + 1)
+    return [[set(range_(*map(int, item.split("-")))) for item in line.split(",")] for line in inp.split("\n")]
+
+def count_containments(pairs: list) -> int:
+    """Counts the amount of pairs where one half wholly contains the other"""
+    return sum((s1.issubset(s2) or s2.issubset(s1) for s1, s2 in pairs))
+        
+
+
+if __name__ == "__main__":
+    testinput = """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"""
+    
+    with open("04/input.txt") as file:
+        print(count_containments(get_section_ids(file.read())))
